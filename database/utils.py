8f67459fbccb542250f767dbedeb99c3ae52bf7e
Clement-O <o.clems@hotmail.fr>
2020-01-12 17:56:23 +0100

    doc(db): database docstring and .rst file

--- database/utils.py
+++ database/utils.py
@@ -1,13 +1,23 @@
 # noinspection SqlNoDataSourceInspection,SqlDialectInspection
 class Postgres:
+    """
+    Provides functions to query PostgreSQL
 
-    def __init__(self, table, column):
+    :param str table: Param to select the right table
+    :param str column: Param to search duplicate or unique on
+    """
+
+    def __init__(self, table: str, column: str) -> None:
         self.table = table
         self.column = column
         if not all(isinstance(x, str) for x in [self.table, self.column]):
             raise TypeError(f"The 'table' and/or 'column' must be a string!")
 
-    def select_pk_name_query(self):
+    def select_pk_name_query(self) -> str:
+        """
+
+        :return: Postgres query to select the primary key
+        """
         return f"SELECT a.attname " \
                f"FROM pg_index AS i " \
                f"JOIN pg_attribute AS a " \
@@ -16,7 +26,11 @@ class Postgres:
                f"WHERE  i.indrelid = '{self.table}'::regclass " \
                f"AND i.indisprimary;"
 
-    def select_duplicate_query(self):
+    def select_duplicate_query(self) -> str:
+        """
+
+        :return: Postgres query to select the duplicate entries
+        """
         return f"SELECT t.* " \
                f"FROM {self.table} AS t " \
                f"INNER JOIN (" \
@@ -26,7 +40,12 @@ class Postgres:
                f"HAVING ( COUNT(*) > 1 )" \
                f") dt ON t.{self.column}=dt.{self.column}"
 
-    def select_duplicate_pk_query(self, pk):
+    def select_duplicate_pk_query(self, pk: str) -> str:
+        """
+
+        :param pk: Primary key of the table
+        :return: Postgres query to select the pk of the duplicate entries
+        """
         return f"SELECT t.{pk} " \
                f"FROM {self.table} AS t " \
                f"INNER JOIN (" \
@@ -36,7 +55,11 @@ class Postgres:
                f"HAVING ( COUNT(*) > 1 )" \
                f") dt ON t.{self.column}=dt.{self.column}"
 
-    def select_unique_query(self):
+    def select_unique_query(self) -> str:
+        """
+
+        :return: Postgres query to select the unique entries
+        """
         return f"SELECT t.* " \
                f"FROM {self.table} AS t " \
                f"INNER JOIN (" \
@@ -46,7 +69,12 @@ class Postgres:
                f"HAVING ( COUNT(*) = 1 )" \
                f") dt ON t.{self.column}=dt.{self.column}"
 
-    def select_unique_pk_query(self, pk):
+    def select_unique_pk_query(self, pk: str) -> str:
+        """
+
+        :param pk: Primary key of the table
+        :return: Postgres query to select the pk of the unique entries
+        """
         return f"SELECT t.{pk} " \
                f"FROM {self.table} AS t " \
                f"INNER JOIN (" \
@@ -59,14 +87,24 @@ class Postgres:
 
 # noinspection SqlNoDataSourceInspection,SqlDialectInspection
 class MySQL:
+    """
+    Provides functions to query MySQL
 
-    def __init__(self, table, column):
+    :param str table: Param to select the right table
+    :param str column: Param to search duplicate or unique on
+    """
+
+    def __init__(self, table: str, column: str) -> None:
         self.table = table
         self.column = column
         if not all(isinstance(x, str) for x in [self.table, self.column]):
             raise TypeError(f"The 'table' and/or 'column' must be a string!")
 
-    def select_pk_name_query(self):
+    def select_pk_name_query(self) -> str:
+        """
+
+        :return: MySQL query to select the primary key
+        """
         return f"SELECT k.COLUMN_NAME " \
                f"FROM information_schema.table_constraints t " \
                f"LEFT JOIN information_schema.key_column_usage k " \
@@ -75,7 +113,11 @@ class MySQL:
                f"AND t.table_schema=DATABASE() " \
                f"AND t.table_name='{self.table}'"
 
-    def select_duplicate_query(self):
+    def select_duplicate_query(self) -> str:
+        """
+
+        :return: MySQL query to select the duplicate entries
+        """
         return f"SELECT t.* " \
                f"FROM {self.table} AS t " \
                f"INNER JOIN (" \
@@ -85,7 +127,12 @@ class MySQL:
                f"HAVING ( COUNT(*) > 1 )" \
                f") dt ON t.{self.column}=dt.{self.column}"
 
-    def select_duplicate_pk_query(self, pk):
+    def select_duplicate_pk_query(self, pk: str) -> str:
+        """
+
+        :param pk: Primary key of the table
+        :return: MySQL query to select the pk of the duplicate entries
+        """
         return f"SELECT t.{pk} " \
                f"FROM {self.table} AS t " \
                f"INNER JOIN (" \
@@ -95,7 +142,11 @@ class MySQL:
                f"HAVING ( COUNT(*) > 1 )" \
                f") dt ON t.{self.column}=dt.{self.column}"
 
-    def select_unique_query(self):
+    def select_unique_query(self) -> str:
+        """
+
+        :return: MySQL query to select the unique entries
+        """
         return f"SELECT t.* " \
                f"FROM {self.table} AS t " \
                f"INNER JOIN (" \
@@ -105,7 +156,12 @@ class MySQL:
                f"HAVING ( COUNT(*) = 1 )" \
                f") dt ON t.{self.column}=dt.{self.column}"
 
-    def select_unique_pk_query(self, pk):
+    def select_unique_pk_query(self, pk: str) -> str:
+        """
+
+        :param pk: Primary key of the table
+        :return: MySQL query to select the pk of the unique entries
+        """
         return f"SELECT t.{pk} " \
                f"FROM {self.table} AS t " \
                f"INNER JOIN (" \
---
+++ docs/python-duplicate/database.rst
@@ -0,0 +1,39 @@
+.. _database:
+
+Database Documentation:
+=======================
+
+This document specifies python-duplicate's database package.
+
+FromPSQL
+--------
+
+.. module:: database.from_psql
+
+.. autoclass:: FromPSQL
+    :members:
+
+FromMySQL
+---------
+
+.. module:: database.from_mysql
+
+.. autoclass:: FromMySQL
+    :members:
+
+Utils
+-----
+
+Postgres
+^^^^^^^^
+
+.. module:: database.utils
+
+.. autoclass:: Postgres
+
+MySQL
+^^^^^
+
+.. module:: database.utils
+
+.. autoclass:: MySQL
